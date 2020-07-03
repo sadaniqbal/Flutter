4eedf5c1978a534f5c8ccb586c1b80618f52691a
Sadan Iqbal <sadaniqbal@gmail.com>
2020-07-03 18:59:18 +0500

    Changing MyAppState public class to private class

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -9,11 +9,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
