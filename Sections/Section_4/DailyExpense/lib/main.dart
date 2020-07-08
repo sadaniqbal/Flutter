86834ee8656c1030b82b3cd39a2b90f09edf006d
Sadan Iqbal <sadaniqbal@gmail.com>
2020-07-09 00:46:14 +0500

    Uptil step 72 (Section 4)

---
+++ Sections/Section_4/DailyExpense/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Expense'),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.green,
+                child: Text('Chart1'),
+                elevation: 10,
+              ),
+            ),
+            Card(color: Colors.red, child: Text('LIST OF TX '))
+          ]),
+    );
+  }
+}
