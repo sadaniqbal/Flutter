09cd154cb02abfc2ccab6084a38a8ac689f2891c
Sadan Iqbal <sadaniqbal@gmail.com>
2020-07-11 21:58:05 +0500

    Uptil Step 84 (Sect 4) - Before Splitting Wigdets

--- Sections/Section_4/DailyExpense/lib/main.dart
+++ Sections/Section_4/DailyExpense/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
+import 'package:DailyExpense/transaction.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -16,6 +20,25 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Shoes',
+      amount: 60,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Tomatoes',
+      amount: 30,
+      date: DateTime.now(),
+    ),
+  ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,18 +46,100 @@ class MyHomePage extends StatelessWidget {
         title: Text('Daily Expense'),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
               child: Card(
                 color: Colors.green,
-                child: Text('Chart1'),
+                child: Text('Chart!'),
                 elevation: 10,
               ),
             ),
-            Card(color: Colors.red, child: Text('LIST OF TX '))
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: titleController,
+                        // onChanged: (val) {
+                        //   titleInput = val;
+                        // },
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Amount'),
+                        controller: amountController,
+                        // onChanged: (val) => amountInput = val,
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          // print(titleInput);
+                          // print(amountInput);
+                          print(titleController.text);
+                          print(amountController.text);
+                        },
+                        child: Text('Add Transaction'),
+                        textColor: Colors.purple,
+                      )
+                    ]),
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('MMMM/dd/yyyy').format(tx.date),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.grey,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           ]),
     );
   }
